078fac1c1e3ce95028b3df45001919aa488f9b01
edcorners <esquinas.developer@gmail.com>
2016-09-22 20:53:09 -0400

    chore: Create empty activities and fragments

---
+++ app/src/main/java/com/eddev/android/gamesight/GameDetailActivityFragment.java
@@ -0,0 +1,22 @@
+package com.eddev.android.gamesight;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GameDetailActivityFragment extends Fragment {
+
+    public GameDetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game_detail, container, false);
+    }
+}
---
+++ app/src/main/java/com/eddev/android/gamesight/GameGridActivityFragment.java
@@ -0,0 +1,22 @@
+package com.eddev.android.gamesight;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GameGridActivityFragment extends Fragment {
+
+    public GameGridActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game_grid, container, false);
+    }
+}
