fbd82add2b2829f67a295de17058a63be3194668
edcorners <esquinas.developer@gmail.com>
2016-09-24 22:19:26 -0400

    chore: Add 'game' service

--- app/src/main/java/com/eddev/android/gamesight/client/giantbomb/GiantBombService.java
+++ app/src/main/java/com/eddev/android/gamesight/client/giantbomb/GiantBombService.java
@@ -4,7 +4,10 @@ import com.eddev.android.gamesight.client.giantbomb.model.Games;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
+
+import static android.R.attr.format;
 
 /**
  * Created by Edison on 9/22/2016.
@@ -14,6 +17,13 @@ public interface GiantBombService {
     @GET("games")
     Call<Games> getGames(@Query("format") String format,
                          @Query("filter") String filter,
+                         @Query("sort") String sort,
+                         @Query("limit") String limit,
                          @Query("api_key") String apiKey);
-    //TODO add full list of params
+
+    @GET("game/{id}")
+    Call<Games> getGame(@Path("id") int gameId,
+                        @Query("format") String format,
+                        @Query("filter") String filter,
+                        @Query("api_key") String apiKey);
 }
