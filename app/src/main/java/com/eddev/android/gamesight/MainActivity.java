71c536110fda6633418ef4bcb859904a76c4bc4f
edcorners <esquinas.developer@gmail.com>
2016-10-15 17:52:48 -0400

    feat: Configure searchable activity, add btn

--- app/src/main/java/com/eddev/android/gamesight/MainActivity.java
+++ app/src/main/java/com/eddev/android/gamesight/MainActivity.java
@@ -70,7 +70,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_search);
 
         return true;
     }
--- app/src/main/java/com/eddev/android/gamesight/SearchActivity.java
+++ app/src/main/java/com/eddev/android/gamesight/SearchActivity.java
@@ -1,5 +1,7 @@
 package com.eddev.android.gamesight;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //(query);
+        }
     }
 }
